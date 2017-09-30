ba3e46183d5369d51db4ea08b36b7ded31ae5725
Luiz Guilherme Ferrari <lgm.ferrari@hotmail.com>
2017-09-30 18:04:17 -0300

    AngularFire2 working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  items: Observable<any[]>;
-  constructor(db: AngularFireDatabase) {
-    this.items = db.list('items');
+export class AppComponent implements OnInit, OnDestroy {
+  items: any[];
+  subscription: Subscription;
+
+  constructor(private db: AngularFireDatabase) { }
+
+  ngOnInit(): void {
+    this.subscription = this.db.list('items/negatives').subscribe(rs => this.items = rs);
+
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
