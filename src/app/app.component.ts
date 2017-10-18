f72124440b0017357f3b77c6b13cc20537e5f293
Marcos Vinicius Jacobs <mvmjacobs@gmail.com>
2017-10-18 00:06:53 -0200

    refactor(core): move user model to /core/model
    Refactor in folder structure to follow the guide
    
    Closes #8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
-import { User } from './shared/login/user.model';
+import { User } from './core/model/user.model';
 
 @Component({
 	selector: 'app-root',
---
+++ src/app/core/model/user.model.ts
@@ -0,0 +1,8 @@
+export class User {
+	constructor(
+		public uid?: string,
+		public Name?: string,
+		public Email?: string,
+		public Password?: string
+	) { }
+}
